1739832cfd7556d489ddfed4104c039a57ef8f02
EnricoCiccolini <piroroma90@gmail.com>
2025-06-27 15:10:23 +0200

    feat: new exe complete

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,15 +1,21 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
 
-        Prodotto collector = new Prodotto("Magic", "pacchetto del gioco magic the gathering", 8, 22);
-        Prodotto milk = new Prodotto("Milk", "a box of milk", 1.5f, 2);
-
+        Prodotto collector = new Prodotto("magic", "magic", new BigDecimal(60), new BigDecimal(0.22));
+        // Prodotto milk = new Prodotto("Milk", "a box of milk", 1.5f, 2);
+        // Prodotto parmigiano = new Prodotto("parmigiano", "LOREM", 1699.50f, 22);
         System.out.println("il nome completo é " + collector.nameComplete());
 
         System.out.println("il prezzo senza iva " + collector.priceDettail());
         System.out.println("il prezzo con iva è " + collector.priceAndIva());
 
+        // System.out.println("il nome completo é " + parmigiano.nameComplete());
+
+        // System.out.println("il prezzo senza iva " + parmigiano.priceDettail());
+        // System.out.println("il prezzo con iva è " + parmigiano.priceAndIva());
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,33 +1,81 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Random;
 
 public class Prodotto {
-    int barcode;
-    String name;
-    String description;
-    float price;
-    int iva;
+    private int barcode;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private BigDecimal iva;
 
-    public Prodotto(String name, String description, float price, int iva) {
-        Random random = new Random();
+    Random random = new Random();
+
+    public Prodotto() {
+        this.name = "";
+        this.brand = "";
+        this.price = null;
+        this.iva = null;
+        this.barcode = random.nextInt(90000) + 10000;
+    }
+
+    public Prodotto(String name, String brand, BigDecimal price, BigDecimal iva) {
+
         this.name = name;
-        this.description = description;
+        this.brand = brand;
         this.price = price;
         this.iva = iva;
         this.barcode = random.nextInt(90000) + 10000;
 
     }
 
-    public float priceDettail() {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setIva(BigDecimal iva) {
+        this.iva = iva;
+    }
+
+    public BigDecimal getIva() {
+        return iva;
+    }
+
+    public int getBarcode() {
+        return barcode;
+    }
+
+    public void setbrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getbrand() {
+        return brand;
+    }
+
+    public BigDecimal priceDettail() {
         // System.out.println("il prezzo base é");
         // System.out.println(this.price);
         return this.price;
     }
 
-    public float priceAndIva() {
+    public BigDecimal priceAndIva() {
         // System.out.println("il prezzo con iva è");
-        float totalPrice = this.price + ((this.price * this.iva) / 100);
+        BigDecimal totalPrice = this.price.add(this.price.multiply(this.iva)).setScale(2, RoundingMode.DOWN);
         // System.out.println(totalPrice);
         return totalPrice;
     }
@@ -35,7 +83,7 @@ public class Prodotto {
     public String nameComplete() {
         // System.out.println("il nome completo é");
         // System.out.println(this.barcode + this.name);
-        String nameCom = this.barcode + this.name;
+        String nameCom = this.barcode + "-" + this.name;
         return nameCom;
     }
 
